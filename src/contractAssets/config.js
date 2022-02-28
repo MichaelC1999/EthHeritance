a0b9fe37c898f3f63c7414f3bc7d7e86e224895f
Michael <michaelcarroll1999@gmail.com>
2022-02-28 09:26:44 -0500

    Minor changes

--- src/contractAssets/config.js
+++ src/contractAssets/config.js
@@ -1,6 +1,6 @@
 
-  export const InheritorContractAddr = "0xf5dbC25ab3bbB1D6823955591c9A3C7028278E52"
-  export const HeirCoinContractAddr = "0x9Bd2C3EE4f9bA6eA6bd7313FfCfDfDEfa22dA017"
+  export const InheritorContractAddr = "0x250A3fB735Ee0568276330853d67Ba82927BaED2"
+  export const HeirCoinContractAddr = "0xF5827eA5F8FFB813b2509c066c70Fba1c65B9707"
   export const DeployedFrom = "0x79c9437Fc626346A18b8661B370e929d02FFb57b"
   export const chainID = "4"
   
